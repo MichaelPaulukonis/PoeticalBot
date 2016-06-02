de6b810f21e1b7b7c52e01f1a4d0495a78171e1c
Michael Paulukonis <xraysmalevich@gmail.com>
2016-06-02 11:30:15 -0400

    option/params (incl. command-line) for text, poem method, corpus filter. bug-fix on weightsTentpole

--- writepoem.js
+++ writepoem.js
@@ -6,23 +6,33 @@ let program = require('commander'),
 program
   .version('0.0.2')
   .option('-l, --log', 'dump to log')
-  .option('-t, --transform [percent]', 'percent chance of text transform (in hundreths, eg 0.25 = 25%')
+  .option('-x, --xform [percent]', 'percent chance of text transform (in hundreths, eg 0.25 = 25%')
   .option('-m, --method [jgnoetry, buckets]', 'specify poem generate method')
+  .option('-t, --templateName [jgnoetry template name]', 'specify jgnoetry template to use')
+  .option('-c, --corporaFilter [string]', 'filename substring filter (non-case sensitive)')
   .parse(process.argv);
 
 if (program.log) {
   config.log = true;
 }
 
-if (program.transform) {
-  let chance = parseFloat(program.transform, 10);
+if (program.xform) {
+  let chance = parseFloat(program.xform, 10);
   if (!isNaN(chance)) {
     config.transformChance = chance;
   }
 }
 
+if (program.templateName) {
+  config.templateName = program.templateName;
+}
+
 if (program.method) {
   config.method = program.method;
+}
+
+if (program.corporaFilter) {
+  config.corporaFilter = program.corporaFilter;
 }
 
 let poetifier = new require('./lib/poetifier.js')({config: config});
