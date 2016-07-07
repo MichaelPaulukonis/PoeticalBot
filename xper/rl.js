e5152333f3a1a03ab286bff720dcc9f96e0e9adb
Michael Paulukonis <xraysmalevich@gmail.com>
2016-07-06 23:26:14 -0400

    more reduction play. think we are almost there

--- xper/rl.js
+++ xper/rl.js
@@ -6,7 +6,8 @@ let util = new require('../lib/util.js')(),
     corpora = new Corpora(),
     lines = { lines: [] },
     nlp = require('nlp_compromise'),
-    nlpNgram = require('nlp-ngram');
+    nlpNgram = require('nlp-ngram'),
+    reduceType = require('../lib/linereduce.js').types;
 
 nlp.plugin(nlpNgram);
 let text1 = util.pick(corpora.texts),
@@ -14,26 +15,43 @@ let text1 = util.pick(corpora.texts),
     text = text1.text() + ' ' + text2.text(),
     name = text1.name + ' ' + text2.name;
 
-timer.start(name, true);
-let t = nlp.text(text);
-var ng = t.ngram({min_count: 10})
-      .filter(n => n[0] && n[0].size && n[0].size > 2)
-      .reduce((p,c) => p.concat(c))
-      .filter(w => w.word.trim().length > 0);
-// console.log(JSON.stringify(ng));
-timer.stop(name, true);
-
-let search = util.pick(ng).word;
-
-console.log(`search: ${search}`);
-
-let linereduce = new require('../lib/linereduce.js')({
+let linereduce = new require('../lib/linereduce.js').LineReduce({
   util: util,
-  text: text,
-  search: search
+  text: text
 });
 
-lines = linereduce.filter();
+switch (util.pick([1,2,3])){
+
+case 1:
+  lines = linereduce.filter({type: reduceType.start});
+  break;
+
+case 2:
+  lines = linereduce.filter({type: reduceType.end});
+  break;
+
+case 3:
+default:
+  timer.start(name, true);
+  // make this part of linereduce
+  // why should it happen out here?
+  // ANSWER: because linereduce has no need for this gibberish
+  let t = nlp.text(text);
+  var ng = t.ngram({min_count: 10})
+        .filter(n => n[0] && n[0].size && n[0].size > 1)
+        .reduce((p,c) => p.concat(c))
+        .filter(w => w.word.trim().length > 2);
+  // console.log(JSON.stringify(ng));
+  timer.stop(name, true);
+
+  let search = util.pick(ng).word;
+
+  console.log(`search: ${search}`);
+
+  lines = linereduce.filter({search: search});
+  break;
+}
+
 
 let config = {util: util, texts: [ {name: 'lines', text: () => lines.lines.join('\n')}] },
     buckets = require('../lib/buckets'),
@@ -45,15 +63,3 @@ console.log(poem.text);
 
 // TODO: get Moby Dick and Pride and Prejudice in there
 // TODO: poetry of some sort
-
-
-// while (lines.lines.length < 6) {
-//   let text = util.pick(corpora.texts);
-//   timer.start(text.name, true);
-//   let linereduce = new require('../lib/linereduce.js')({util: util, texts: [text]});
-//   lines = linereduce.filter();
-//   timer.stop(text.name, true);
-//   console.log(`length: ${lines.lines.length}`);
-// }
-
-// console.log(lines.lines);
