7bca4d0358b398d71abcb3f865a63c431f55e836
Michael Paulukonis <mpauluko@travelers.com>
2019-04-15 07:55:22 -0400

    Finished linereduce tests Minor changes to linereduce (mostly notes)

--- test/linereduce.tests.js
+++ test/linereduce.tests.js
@@ -2,40 +2,37 @@ var chai = require('chai')
 var dirtyChai = require('dirty-chai')
 var expect = chai.expect
 let util = new (require(`../lib/util.js`))()
-
 chai.use(dirtyChai)
 
-const { linereduce: lr } = require('../lib/')
-console.log(lr)
+// const { linereduce: lr } = require('../lib/')
+const { LineReduce, types } = require('../lib').linereduce
 
-// TODO: absolutely unimplemented
-// look at the runner, and move on from there....
-
-describe.only(`linereduce `, () => {
+describe(`linereduce `, () => {
   describe(`API`, () => {
-    // module.exports = { LineReduce, types: reduceType }
-
     it(`...provides a LineReduce method`, () => {
-      expect(lr.LineReduce).to.be.a(`function`)
+      expect(LineReduce).to.be.a(`function`)
     })
 
     it(`... provides a "list" of reduction types (object)`, () => {
-      expect(lr.types).to.be.an(`object`)
+      expect(types).to.be.an(`object`)
     })
 
     it(`... should throw a TypeError if not provided with a util`, () => {
       expect(function () {
-        (() => new lr.LineReduce())()
+        (() => new LineReduce())()
       }).to.throw(Error)
+    })
+
+    it('... has a filter method once instantiated', () => {
+      const linereduce = LineReduce({ util })
+      expect(linereduce.filter).to.be.a('function')
     })
   })
 
-  // var Util = require(`../lib/util.js`)
-  // var util = new Util()
   describe('... in action', () => {
     let linereduce, blob
     before(() => {
-      linereduce = new lr.LineReduce({ util })
+      linereduce = new LineReduce({ util })
       blob = `This is some text.
 This is some more.
 Another line of text.
@@ -46,16 +43,42 @@ This is maybe enough.
 Enough text?
 This is probably not.`
     })
-    it('should work somehow', () => {
-      const startLines = linereduce.filter({ type: lr.types.start, text: blob })
-      expect(startLines).to.be.an('object')
-      expect(startLines).to.have.property('lines')
-      expect(startLines).to.have.property('text')
+    it('... will get a set of sentences that start with the same word', () => {
+      const firsts = linereduce.filter({ type: types.start, text: blob })
+      expect(firsts).to.be.an('object')
+      expect(firsts).to.have.property('lines')
+      expect(firsts).to.have.property('text')
 
-      const endLines = linereduce.filter({ type: lr.types.end, text: blob })
-      expect(endLines).to.be.an('object')
-      expect(endLines).to.have.property('lines')
-      expect(endLines).to.have.property('text')
+      expect(firsts.lines.length).to.be.greaterThan(0)
+
+      const firstWord = firsts.lines[0].split(' ')[0]
+      const allSame = firsts.lines.reduce((p, line) => p && line.startsWith(firstWord), true)
+      expect(allSame).to.be.true()
+    })
+
+    it('... will get a set of sentences that END with the same word', () => {
+      const lasts = linereduce.filter({ type: types.end, text: blob })
+      expect(lasts).to.be.an('object')
+      expect(lasts).to.have.property('lines')
+      expect(lasts).to.have.property('text')
+
+      expect(lasts.lines.length).to.be.greaterThan(0)
+
+      const lastWord = lasts.lines[0].split(' ').slice(-1)[0]
+      const allSame = lasts.lines.reduce((p, line) => p && line.endsWith(lastWord), true)
+      expect(allSame).to.be.true()
+    })
+
+    it('... will get a set of sentences that match some pattern', () => {
+      const matches = linereduce.filter({ type: types.search, text: blob, search: / is / })
+      expect(matches).to.be.an('object')
+      expect(matches).to.have.property('lines')
+      expect(matches).to.have.property('text')
+
+      expect(matches.lines.length).to.be.greaterThan(0)
+
+      const allSame = matches.lines.reduce((p, line) => p && line.indexOf(' is ') > -1, true)
+      expect(allSame).to.be.true()
     })
   })
 })
